e5202aab9ab5b33321b5a90f96ed9128703a6186
DESKTOP-LAPTOP\Mati <henry131@wp.pl>
2017-09-27 23:07:15 +0200

    variables, tables

--- C_sharp_practice/Program.cs
+++ C_sharp_practice/Program.cs
@@ -10,6 +10,16 @@ namespace C_sharp_practice
     {
         static void Main(string[] args)
         {
+            var tmp = 1.23;
+            Console.WriteLine(tmp);
+            Console.ReadKey();
+
+            int[] tab = new int[6] { 1,2,3,4,5,6};
+            for (int i =0; i<tab.Length; i++)
+            {
+                Console.Write(tab[i]+", ");
+            }
+            Console.ReadKey();
         }
     }
 }
